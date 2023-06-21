be8044623390988a57a787dbc1eb7760f0c80d1c
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-21 13:46:06 +0100

    Dealing with string methods

--- 01_python/base_concepts/variables_and_data_types/name.py
+++ 01_python/base_concepts/variables_and_data_types/name.py
@@ -4,8 +4,21 @@ Date Created: 21-06-2023
 Subject: Understand how strings work in python
 '''
 
+# capitalizing first letter of the work
 name = "alphonse brandon"
 
 print(
     name.title()
+)
+
+# uppercase
+
+print(
+    name.upper()
+)
+
+# lower case
+
+print (
+    name.lower()
 )
