b3763635941aaea801dded970a42ba3060ac0158
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-21 14:06:59 +0100

    Using an apostrope inside in a string

---
+++ 01_python/base_concepts/variables_and_data_types/apostrophe.py
@@ -0,0 +1,10 @@
+'''
+Author: Alphonse Brandon
+Date Created: 21-06-2023
+Subject: Understand how strings work in python
+'''
+
+# Using an apostrope inside in a string
+message = "One of Python's strenghts is its divers community"
+
+print(message)
