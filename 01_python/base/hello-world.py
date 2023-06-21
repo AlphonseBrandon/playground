566ae859ce8567cfcd5c5ba45037a16abe525adf
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-21 13:36:20 +0100

    Simple message

--- 01_python/base/hello-world.py
+++
@@ -1,7 +0,0 @@
-'''
-Author: Alphonse Brandon
-Date Created: 21-06-2023
-Subject: Testing installation
-'''
-
-print('Hello Alphonse!')
---
+++ 01_python/base_concepts/variables_and_data_types/simple_message.py
@@ -0,0 +1,8 @@
+'''
+Author: Alphonse Brandon
+Date Created: 21-06-2023
+Subject: Understand how variables work in python
+'''
+
+message = "Hello Alphonse!"
+print(message)
