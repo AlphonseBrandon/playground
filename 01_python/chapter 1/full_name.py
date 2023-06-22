9fa4ca77e4a9146f6f35e7ae72fa810163af8117
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-22 23:51:56 +0100

    Reorganize into chapters

--- 01_python/chapter 1/full_name.py
+++ 01_python/chapter 1/full_name.py
@@ -4,6 +4,8 @@ Date Created: 21-06-2023
 Subject: Understand how strings work in python
 '''
 
+# Convert this code into a function 
+
 first_name = "alphonse"
 last_name = "brandon"
 
@@ -12,3 +14,6 @@ full_name = f"{first_name} {last_name}"
 message = f"Hello, {full_name}!"
 
 print(message)
+
+ 
+
