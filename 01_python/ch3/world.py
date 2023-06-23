f1cf10ee649e3809c75aed535524999f92866dc3
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-23 08:05:00 +0100

    List comprehension methods

---
+++ 01_python/ch3/world.py
@@ -0,0 +1,51 @@
+'''
+Author: Alphonse Brandon
+Date Created: 22-06-2023
+Subject: List Comprehension
+'''
+
+places = ['usa', 'canada', 'france', 'rwanda', 'south africa']
+
+print(
+    f'\nOriginal List:\n{places}'
+)
+
+alphabetical_order = sorted(places)
+
+print(
+    f'\nAlphabetical Order:\n{alphabetical_order}'
+)
+
+print(
+    f'\nSee the original list has not changed:\n{places}'
+)
+
+places.reverse()
+
+print(
+    f'\nReverse Order:\n{places}'
+)
+
+places.reverse()
+
+print(
+    f'\nBack to Original Order:\n{places}'
+)
+
+places.sort()
+
+print(
+    f'\nPermanent Sort:\n{places}'
+)
+
+places.sort(reverse=True)
+
+print(
+    f'\nReversed Order Sort:\n{places}'
+)
+
+number_of_countries = len(places)
+
+print(
+    f'\nThe number of countries in the list is:\n{number_of_countries}'
+)
