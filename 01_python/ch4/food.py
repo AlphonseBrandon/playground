169fce067cc3817b3e01d34bdc328ca257f1386b
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-24 23:25:55 +0100

    Looping through a list

--- 01_python/ch4/food.py
+++ 01_python/ch4/food.py
@@ -3,8 +3,10 @@ Author: Alphonse Brandon
 Date Created: 22-06-2023
 Subject: List Comprehension
 '''
-
+message = 'I like Cameroonian dishes'
 foods = ['eru', 'rice', 'beans', 'stew']
 
 for food in foods:
-    print(food)
+    print(f'I enjoy {food}\n')
+
+print(message)
