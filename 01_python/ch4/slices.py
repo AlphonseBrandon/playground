a009378c3d2230ebad3ba0f132b454edd14dc676
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-25 22:14:09 +0100

    Slicing through a list

---
+++ 01_python/ch4/slices.py
@@ -0,0 +1,15 @@
+'''
+Author: Alphonse Brandon
+Date Created: 22-06-2023
+Subject: slicing and list copying
+'''
+
+foods = ['beans', 'yam', 'rice', 'meat', 'banana']
+
+message = 'The first three elements in the list are:\n'
+
+print(message)
+
+for food in foods:
+    print(food)
+
