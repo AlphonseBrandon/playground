d3110c4316425394ad3337cb8897687988460855
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-25 22:30:33 +0100

    List slicing

--- 01_python/ch4/slices.py
+++ 01_python/ch4/slices.py
@@ -6,9 +6,46 @@ Subject: slicing and list copying
 
 foods = ['beans', 'yam', 'rice', 'meat', 'banana']
 
-message = 'The first three elements in the list are:'
-
+message = 'Elements in the list are:'
 print(message)
+for food in foods:
+    print(food)
+
+message1 = 'The first three elements in the list are:'
+
+print(message1)
 for food in foods[:3]:
     print(food)
 
+message2 = 'Two items from the middle are:'
+
+print(message2)
+for food in foods[2:4]:
+    print(food)
+
+message3 = 'The last three elements are:'
+
+print(message3)
+for food in foods[-3:]:
+    print(food)
+
+alphonse_foods = foods[:]
+alphonse_foods.append('koki')
+
+message4 = 'Elements in orginal food:'
+print(message4)
+
+for food in foods:
+    print(food)
+
+message5 = 'Elements in Alphonse Fooda:'
+
+print(message5)
+for food in alphonse_foods:
+    print(food)
+
+print('Length of foods:')
+print(len(foods))
+
+print('Length of Alphonse foods')
+print(len(alphonse_foods))
