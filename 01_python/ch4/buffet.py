1c76cd865b38a45c500522ad88d11e748457bd16
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-26 23:52:31 +0100

    Modifying elements in the turple

--- 01_python/ch4/buffet.py
+++ 01_python/ch4/buffet.py
@@ -6,5 +6,12 @@ Subject: Working with turples
 
 foods = ('beans', 'yam', 'rice', 'meat', 'banana')
 
+print('Original Menu:')
+for food in foods:
+    print(food)
+
+foods = ('beans', 'yam', 'rice', 'meat', 'plantain', 'koki')
+
+print('Modified Menu:')
 for food in foods:
     print(food)
