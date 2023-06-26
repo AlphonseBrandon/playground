ed9f1e46ae25ecb27393d49d1e35ce8a487d75dc
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-26 23:49:58 +0100

    Looping through a turple

---
+++ 01_python/ch4/buffet.py
@@ -0,0 +1,10 @@
+'''
+Author: Alphonse Brandon
+Date Created: 26-06-2023
+Subject: Working with turples
+'''
+
+foods = ('beans', 'yam', 'rice', 'meat', 'banana')
+
+for food in foods:
+    print(food)
