136b5d55737f695ca506a81d9bb2314d561ef24a
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-28 23:31:06 +0100

    Conditional test exercise 5-5

--- 01_python/ch5/colors.py
+++ 01_python/ch5/colors.py
@@ -4,9 +4,11 @@ Date Created: 28-06-2023
 Subject: Conditional test with if statements
 '''
 
-alien_color = 'green'
+alien_color = 'yellow'
 
 if alien_color == 'green':
     print('You just earned 5 points!')
-else:
+elif alien_color == 'yellow':
     print('You just earned 10 points!')
+elif alien_color == 'red':
+    print('You just earned 15 points!')
