bf2481a361439b1bb74e2e794077da724b2cf12c
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-28 23:34:43 +0100

    Conditional test exercise 5-6

--- 01_python/ch5/colors.py
+++ 01_python/ch5/colors.py
@@ -12,3 +12,24 @@ elif alien_color == 'yellow':
     print('You just earned 10 points!')
 elif alien_color == 'red':
     print('You just earned 15 points!')
+
+# Stages of life
+
+age = 91
+
+if age < 2:
+    print('You are a baby')
+elif age < 4:
+    print('You are a toddler')
+elif age < 13:
+    print('You are a kid')
+elif age < 20:
+    print('You are a teenager')
+elif age < 65:
+    print('You are an adult')
+elif age >= 65:
+    print('You are an elder')
+
+# Favorite fruit
+
+fruits = ['apple', 'banana', 'orange', 'grape', 'watermelon']
