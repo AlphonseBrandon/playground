cfc3ea1ac041bd911c28ef986860b221c8805819
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-29 00:07:22 +0100

    exercise 5-10

--- 01_python/ch5/admin.py
+++ 01_python/ch5/admin.py
@@ -4,10 +4,28 @@ Date Created: 28-06-2023
 Subject: Combining multiple conditions
 '''
 
-usernames = ['admin', 'user1', 'user2', 'user3', 'user4']
+usernames = ['admin', 'brandon', 'james', 'jessica', 'jennifer']
 
 for username in usernames:
     if username == 'admin':
         print('Hello admin, would you like to see a status report?')
     else:
         print(f'Hello {username}, thank you for logging in again')
+no_users = []
+
+# check if list is empty
+if no_users:
+    for no_user in no_users:
+        print(f'Hello {no_user}, thank you for logging in again')
+else:
+    print('We need to find some users!')
+
+current_users = ['admin', 'brandon', 'james', 'jessica', 'jennifer']
+
+new_users = ['admin', 'brandon', 'james', 'jessica', 'jennifer', 'joseph']
+
+for new_user in new_users:
+    if new_user in current_users:
+        print(f'The username {new_user} has already been used')
+    else:
+        print(f'The username {new_user} is available') 
