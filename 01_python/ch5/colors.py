dace075a7db745fc3bd57f8fa782424583bbcb85
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-28 23:39:05 +0100

    Conditional test exercise 5-7

--- 01_python/ch5/colors.py
+++ 01_python/ch5/colors.py
@@ -33,3 +33,16 @@ elif age >= 65:
 # Favorite fruit
 
 fruits = ['apple', 'banana', 'orange', 'grape', 'watermelon']
+
+favourite_fruits = fruits[:3]
+
+if 'apple' in favourite_fruits:
+    print('You really like apples!')
+if 'banana' in favourite_fruits:
+    print('You really like bananas!')
+if 'orange' in favourite_fruits:
+    print('You really like oranges!')
+if 'grape' in favourite_fruits:
+    print('You really like grapes!')
+if 'watermelon' in favourite_fruits:
+    print('You really like watermelons!')
