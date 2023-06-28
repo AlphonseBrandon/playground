8fa24f4e173d5da236b8e775702744ec4b78b762
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-29 00:29:05 +0100

    exercise 5-11

--- 01_python/ch5/admin.py
+++ 01_python/ch5/admin.py
@@ -7,7 +7,7 @@ Subject: Combining multiple conditions
 usernames = ['admin', 'brandon', 'james', 'jessica', 'jennifer']
 
 for username in usernames:
-    if username == 'admin':
+    if username is 'admin':
         print('Hello admin, would you like to see a status report?')
     else:
         print(f'Hello {username}, thank you for logging in again')
@@ -22,10 +22,24 @@ else:
 
 current_users = ['admin', 'brandon', 'james', 'jessica', 'jennifer']
 
-new_users = ['admin', 'brandon', 'james', 'jessica', 'jennifer', 'joseph']
+new_users = ['admin', 'Brandon', 'james', 'jessica', 'jennifer', 'joseph']
 
 for new_user in new_users:
     if new_user in current_users:
         print(f'The username {new_user} has already been used')
     else:
         print(f'The username {new_user} is available') 
+
+# Ordinal numbers 
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for number in numbers[:4]:
+    if number is numbers[0]:
+        print(f'{number}st')
+    if number is numbers[1]:
+        print(f'{number}nd')
+    if number is numbers[2]:
+        print(f'{number}rd')
+for number in numbers[3:]:
+    print(f'{number}th')
