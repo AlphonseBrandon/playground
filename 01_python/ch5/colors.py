a18ec4d6c43bbfea4ceba57513f633789623a330
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-28 23:27:28 +0100

    Conditional test exercise

---
+++ 01_python/ch5/colors.py
@@ -0,0 +1,10 @@
+'''
+Author: Alphonse Brandon
+Date Created: 28-06-2023
+Subject: Conditional test with if statements
+'''
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('You just earned 5 points!')
