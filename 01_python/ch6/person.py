6f9c6ec71cb7980840d5726339e14802dcd93ad1
AlphonseBrandon <alphonse.brand@gmail.com>
2023-06-29 22:21:30 +0100

    Exercise 6-1

---
+++ 01_python/ch6/person.py
@@ -0,0 +1,14 @@
+'''
+Author: Alphonse Brandon
+Date Created: 28-06-2023
+Subject: Working with dictionaries
+'''
+
+person = {
+    'first_name' : 'alphonse',
+    'last_name' : 'brandon',
+    'age' : 30,
+    'city' : 'buea',
+}
+
+print(f'First Name : {person["first_name"].title()}')
