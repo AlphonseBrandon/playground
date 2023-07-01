8aee6eb53c86a25c1409962d1387ef08b349ed95
AlphonseBrandon <alphonse.brand@gmail.com>
2023-07-01 22:47:40 +0100

    Exercise 6-7

---
+++ 01_python/ch6/people.py
@@ -0,0 +1,34 @@
+'''
+Author: Alphonse Brandon
+Date Created: 01-07-2023
+Subject: Working with dictionaries
+'''
+
+person_one = {
+    'first_name' : 'alphonse',
+    'last_name' : 'brandon',
+    'age' : 30,
+    'city' : 'buea',
+}
+
+person_two = {
+    'first_name' : 'john',
+    'last_name' : 'doe',
+    'age' : 39,
+    'city' : 'chicago',
+}
+
+person_three = {
+    'first_name' : 'marry',
+    'last_name' : 'ann',
+    'age' : 18,
+    'city' : 'texas',
+}
+
+people = [person_one, person_two, person_three]
+
+for person in people:
+    print("User Info\n")
+    for key, value in person.items():
+        print(f'{key} : {value}\n')
+        
