748d3793b596894a18a7e9cfc2df1bc15d6c2411
AlphonseBrandon <alphonse.brand@gmail.com>
2023-07-02 13:48:13 +0100

    Exercise 7-1

---
+++ 01_python/ch7/rental_car.py
@@ -0,0 +1,11 @@
+'''
+Author: Alphonse Brandon
+Date Created: 02-07-2023
+Subject: Ch6 - Working with dictionaries
+'''
+
+# Rental car
+prompt = 'What kind of rental car will you like?'
+car = input(prompt)
+message = f'Let me see if I can find you a {car}'
+print(message)
