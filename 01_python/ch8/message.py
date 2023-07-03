c77224196259182c13e8fbfb1bc15ec179ee7d4d
AlphonseBrandon <alphonse.brand@gmail.com>
2023-07-03 13:21:18 +0100

    Exercise 8-1

---
+++ 01_python/ch8/message.py
@@ -0,0 +1,10 @@
+'''
+Author: Alphonse Brandon
+Date Created: 03-07-2023
+Subject: Ch6 - Working functions
+'''
+
+def display_message():
+    print('Hi, I am currently learning Python!')
+
+display_message()
