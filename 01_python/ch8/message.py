cc89dd543056127dfa056eb93f92104ce832a6dd
AlphonseBrandon <alphonse.brand@gmail.com>
2023-07-03 16:38:10 +0100

    Exercise 8-8

--- 01_python/ch8/message.py
+++ 01_python/ch8/message.py
@@ -32,10 +32,41 @@ print(country_city)
 
 print('\nAlbum dictionary')
 
-def make_album(name='Loveworld Singers', title='Praise Night 14'):
-    album = {name:title}
+def make_album(name='Loveworld Singers', title='Praise Night 14', songs=None):
+    album = {}
+    if songs:
+        album['name'] = name
+        album['title'] = title
+        album['songs'] = songs
+    else:
+        album['name'] = name
+        album['title'] = title
     return album
 
 album = make_album()
 print(album)
+album = make_album('Choir', 'Praise Service', 13)
+print(album)
 print(f'The data structure is of type: {type(album)}')
+
+
+
+def orders():
+    while True:
+        orders = {}
+        print('To quit the program, enter "q"')
+        name = input('Enter your name: ')
+        if name == 'q':
+            break
+        food = input("what's your favourite food?")
+        if food == 'q':
+            break
+        orders['name'] = name
+        orders['food'] = food
+        return orders
+        
+orders = orders()
+print(orders)
+print('Food ordered')
+for name, food in orders.items():
+    print(f'{orders[name]} your order has been confirmed')
