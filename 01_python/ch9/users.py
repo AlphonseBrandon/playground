57ef8802762a1c011677de2a9dcc3067edba2aad
AlphonseBrandon <alphonse.brand@gmail.com>
2023-07-06 15:30:16 +0100

    Exercise 9-3

---
+++ 01_python/ch9/users.py
@@ -0,0 +1,27 @@
+'''
+Author: Alphonse Brandon
+Date Created: 06-07-2023
+Subject: Ch9 - Working with clases
+'''
+
+class Users:
+    def __init__(self, first_name: str, last_name: str, location: str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.location = location
+
+    def describe_user(self) -> None:
+        '''Display user information'''
+
+        print("Users Information")
+        print(f'\tFirst Name: {self.first_name}\n\tLast name: {self.last_name}\n\tLocation: {self.last_name}')
+    
+    def greet_user(self) -> None:
+        '''Display personalize greeting to user'''
+
+        print(f'Hello {self.first_name} how is the weather in {self.location} today')
+user1 = Users('Alphonse', 'Brandon', 'Buea')
+
+user1.describe_user()
+user1.greet_user()
+    
