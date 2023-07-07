d716a72e11a207526846e60af812e930c0561939
AlphonseBrandon <alphonse.brand@gmail.com>
2023-07-07 23:55:58 +0100

    Reading from a file

--- 01_python/ch10/pi_digits.py
+++ 01_python/ch10/pi_digits.py
@@ -3,3 +3,7 @@ Author: Alphonse Brandon
 Date Created: 06-07-2023
 Subject: Ch9 - Working with Files and exceptions
 '''
+
+with open('pi_digits.txt') as file_object:
+    content = file_object.read()
+    print(content)
