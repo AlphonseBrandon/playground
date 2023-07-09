dae20ce626260247874ae27d0d81abd2b2a333de
AlphonseBrandon <alphonse.brand@gmail.com>
2023-07-09 23:29:59 +0100

    Exercise 8-6

--- 01_python/ch8/city_names.py
+++ 01_python/ch8/city_names.py
@@ -3,3 +3,10 @@ Author: Alphonse Brandon
 Date Created: 09-07-2023
 Subject: Ch6 - Working python functions
 '''
+
+def city_country(city: str, country: str) -> str:
+    '''Print neatly formated pair of city, country'''
+    pair = print(f'{city.title()}, {country.title()}')
+    return pair
+
+city_country('Buea', 'Cameroon')
