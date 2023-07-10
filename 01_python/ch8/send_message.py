85f03ccc8b851f5f928c050b5a3b72e062b36cd4
AlphonseBrandon <alphonse.brand@gmail.com>
2023-07-10 13:03:27 +0100

    Exercise 8-10

--- 01_python/ch8/send_message.py
+++ 01_python/ch8/send_message.py
@@ -10,4 +10,18 @@ def show_messages(drafts: list) -> None:
     """Print each text message stored in drafts"""
     for draft in drafts:
         print(draft)
-show_messages(drafts=drafts)
+
+sent_messages = []
+
+def send_messages(drafts: list) -> None:
+    "Move the messages from draft to send_messages list"
+    while drafts:
+        message = drafts.pop()
+        print(f'Sending {message}')
+        sent_messages.append(message)
+
+send_messages(drafts=drafts)
+
+print(f'List of draft messages:\n{drafts}')
+print(f'List of sent messages:\n{sent_messages}')
+
