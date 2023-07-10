56e579759e8ee7569c5cf3dca260cba2b4febcaa
AlphonseBrandon <alphonse.brand@gmail.com>
2023-07-10 15:27:00 +0100

    Exercise 9-12

---
+++ 01_python/ch9/multiple_modules.py
@@ -0,0 +1,11 @@
+'''
+Author: Alphonse Brandon
+Date Created: 10-07-2023
+Subject: Ch9 - Working with clases and intances of classes
+'''
+
+from admin import Admin
+
+alphonse = Admin('Njei', 'Afuh', 'Texas')
+
+alphonse.privileges.show_privileges()
