05faf3a735f5aa957920787ec3c6a080f1eab6bc
AlphonseBrandon <alphonse.brand@gmail.com>
2023-07-10 15:19:52 +0100

    Exercise 9-8

--- 01_python/ch9/admin.py
+++ 01_python/ch9/admin.py
@@ -9,12 +9,19 @@ import users as us
 class Admin(us.Users):
     def __init__(self, first_name: str, last_name: str, location: str):
         super().__init__(first_name, last_name, location)
+        self.privileges = Privileges()
+
+    
+
+class Privileges:
+    def __init__(self) -> None:
         self.privileges = ['can post', 'can delete', 'can ban user']
 
     def show_privileges(self) -> None:
         """List the privileges of the user"""
         print(self.privileges)
 
+
 user2 = Admin('Alphonse', 'Brandon', 'Buea')
 
-user2.show_privileges()
+user2.privileges.show_privileges()
