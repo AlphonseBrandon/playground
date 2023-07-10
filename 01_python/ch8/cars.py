7d59bec9ba70cfbb05e18f97264d132b120ea0e4
AlphonseBrandon <alphonse.brand@gmail.com>
2023-07-10 13:50:05 +0100

    Exercise 8-15

--- 01_python/ch8/cars.py
+++ 01_python/ch8/cars.py
@@ -1,10 +1,8 @@
 '''
 Author: Alphonse Brandon
-Date Created: 09-07-2023
+Date Created: 10-07-2023
 Subject: Ch6 - Working python functions
 '''
-
-
 
 def make_car(manufacturer: str, model_name: str, **kwargs) -> dict:
     car_dict = {'Manufacture' : manufacturer, 'Model_name' : model_name, **kwargs}
---
+++ 01_python/ch8/printing_model.py
@@ -0,0 +1,11 @@
+'''
+Author: Alphonse Brandon
+Date Created: 10-07-2023
+Subject: Ch6 - Working python functions
+'''
+
+from cars import make_car
+
+car_made = make_car('subaru', 'outback', color='blue',)
+
+print(car_made)
